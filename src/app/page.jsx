2e09db7db59a60230a50f345adc764d461a5c5a2
ozodbek <nozodbek572@gmail.com>
2025-03-06 11:24:29 +0500

    update ' to &apos;

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="container mx-auto">
-       <p className="bb text-4xl font-bold text-center mt-40"> Welcome to nazirov's website</p>
+       <p className="bb text-4xl font-bold text-center mt-40"> Welcome to nazirov&apos;s website</p>
     </div>
   );
 }
